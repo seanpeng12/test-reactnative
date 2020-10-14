54c70252741754b335624c5bf6fe42bb33fab834
seanpeng12 <seanpeng12@gmail.com>
2020-10-14 10:01:55 +0800

    實作stack完成

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 // 第一個頁面
-function HomeScreen() {
+function HomeScreen({ navigation }) {
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        title="前往副頁面"
+        onPress={() => navigation.navigate('Details')}
+
+      />
     </View>
   );
 
 }
 
 // 第二個頁面
-function DetailsScreen() {
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button
+        title="前往主頁面"
+        onPress={() => navigation.navigate('Home')}
+
+      />
     </View>
   );
 }
